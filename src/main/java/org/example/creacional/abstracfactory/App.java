525f43fbe9113dafbbc70034a69813bf21d36fe5
Marcos-antt <62682507+Marcos-antt@users.noreply.github.com>
2023-03-24 14:20:26 -0300

    Update App.java

--- src/main/java/org/example/creacional/abstracfactory/App.java
+++ src/main/java/org/example/creacional/abstracfactory/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) {
         AbstractFactory abstractFactory;
 
-        //gato preto
+        //Cachorro Caramelo 
         abstractFactory = FactoryFornecedor.getFactory("Animal");
         Animal animal = (Animal) abstractFactory.criar("Cachorro");
 
